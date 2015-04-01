433f147ea1efd680e4fbf4199ce1639ee8e63458
Flavio Castelli <fcastelli@suse.com>
2015-04-01 12:09:19 +0200

    Other changes required by ruby 1.8

--- lib/changelog_generator.rb
+++ lib/changelog_generator.rb
@@ -4,6 +4,6 @@ require 'rpm'
 require 'optparse'
 require 'ostruct'
 
-require_relative 'changelog_generator/changelog'
-require_relative 'changelog_generator/options'
-require_relative 'changelog_generator/reporter'
+require File.expand_path('../changelog_generator/changelog', __FILE__)
+require File.expand_path('../changelog_generator/options', __FILE__)
+require File.expand_path('../changelog_generator/reporter', __FILE__)
--- test/changelog_test.rb
+++ test/changelog_test.rb
@@ -1,4 +1,4 @@
-require_relative 'test_helper'
+require File.expand_path('../test_helper', __FILE__)
 
 class ChangelogGeneratorTest < MiniTest::Test
   describe 'changelog generation' do
