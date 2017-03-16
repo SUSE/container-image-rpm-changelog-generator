b944978c708ed501c3eb0b9bba3a63233a97280d
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2017-03-16 19:44:16 +0100

    remove rpmdev-vercmp
    rpmdev-vercmp is a python script that was being used instead of using
    ruby-rpm-ffi gem because of compatibility issues with old sle11sp3

--- lib/changelog_generator.rb
+++ lib/changelog_generator.rb
@@ -6,8 +6,8 @@ end
 require 'erb'
 require 'optparse'
 require 'ostruct'
+require 'rpm'
 
-require File.expand_path('../changelog_generator/rpm/version', __FILE__)
 require File.expand_path('../changelog_generator/changelog', __FILE__)
 require File.expand_path('../changelog_generator/options', __FILE__)
 require File.expand_path('../changelog_generator/reporter', __FILE__)
